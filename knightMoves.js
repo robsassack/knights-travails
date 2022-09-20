c74ddbe31ea92db368675f1be2c50942f71e8353
Rob Sassack <rsassack25@gmail.com>
2022-09-20 02:47:02 -0400

    Added function to check for a valid move

--- knightMoves.js
+++ knightMoves.js
@@ -1,3 +1,10 @@
+function validMove(move) {
+  if (move[0] < 0 || move[0] > 7 || move[1] < 0 || move[1] > 7) {
+    return false;
+  }
+  return true;
+}
+
 function knightMoves(start, end) {
   let board = Array(8).fill().map(() => Array(8).fill('0'));
 }
