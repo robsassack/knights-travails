efc0a4ee2e4675861268c01b2a0f37574a60d3ed
Rob Sassack <rsassack25@gmail.com>
2022-09-20 21:23:18 -0400

    Set up to return fewest steps to destination

--- knightMoves.js
+++ knightMoves.js
@@ -1,3 +1,5 @@
+const possibleMoves = [[2, -1], [2, 1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]];
+
 function validMove(move) {
   if (move[0] < 0 || move[0] > 7 || move[1] < 0 || move[1] > 7) {
     return false;
@@ -6,7 +8,26 @@ function validMove(move) {
 }
 
 function knightMoves(start, end) {
-  let board = Array(8).fill().map(() => Array(8).fill('0'));
+  let queue = [];
+  queue.push(start);
+  let visited = Array(8).fill().map(() => Array(8));
+  visited[start[0]][start[1]] = 1;
+  let moves = Array(8).fill().map(() => Array(8).fill(0));
+  while (queue.length > 0) {
+    let current = queue.shift();
+    if (current[0] === end[0] && current[1] === end[1]) {
+      console.log(moves[current[0]][current[1]]);
+      return moves[current[0]][current[1]];
+    }
+    for (let i=0; i<possibleMoves.length; i++) {
+      let move = [current[0] + possibleMoves[i][0], current[1] + possibleMoves[i][1]];
+      if (validMove(move) && !visited[move[0]][move[1]]) {
+        visited[move[0]][move[1]] = 1;
+        moves[move[0]][move[1]] = moves[current[0]][current[1]] + 1;
+        queue.push(move);
+      }
+    }
+  }
 }
 
-console.log(knightMoves([3, 3], [4, 3]));
+knightMoves([0,0], [5,5]);
